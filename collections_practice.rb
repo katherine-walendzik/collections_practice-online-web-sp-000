4a30e67cb06b1ad66de7416d194547ff431ad4ce
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-27 19:02:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -27,7 +27,7 @@ def reverse_array(array)
 end
 
 def kesha_maker(array)
-  array.each {|word| word[2] = "$"}
+  array.each{|word| word[2] = "$"}
 end
 
 def find_a(array)
@@ -39,5 +39,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  
+  array.each{|word| word + "s"}
 end
